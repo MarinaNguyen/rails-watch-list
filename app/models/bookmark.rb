90c36a4af5ff8c4e6e43c99048f990c2830e8853
Marina Nguyen <nguyenmarina3@gmail.com>
2023-11-30 11:41:42 +0100

    part 1 - Models rspec all green

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, presence: true
+  validates :list_id, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
